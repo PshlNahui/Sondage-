8e996739827bf5674422bdba7baa891826190f1e
Antoine <116713653+PshlNahui@users.noreply.github.com>
2023-02-26 21:45:27 +0100

    Add files via upload

---
+++ sondage/questions.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+    question: "Who is the father of HTML?",
+    a: "Rasmus Lerdorf",
+    b: "Tim Berners-Lee",
+    c: "Brendan Eich",
+    d: "Sergey Brin",
+    correct: "b",
+    },
+    {
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Hypertext Markdown Language",
+    c: "Hyperloop Machine Language",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+    },
+    {
+    question: "What does CSS stand for?",
+    a: "Central Style Sheets",
+    b: "Cascading Style Sheets",
+    c: "Cascading Simple Sheets",
+    d: "Cars SUVs Sailboats",
+    correct: "d",
+    },
+    {
+    question: "Qui est Napoléon Bonaparte ?",
+    a: "général",
+    b: "empereur",
+    c: "roi",
+    d: "militaire",
+    correct: "b",
+    },
+    ];
